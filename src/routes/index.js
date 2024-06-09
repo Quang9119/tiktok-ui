30c53097fc8024cb7bbab0ce45187c11de4f692f
Quang9119 <nguyenminhquangquangquang@gmail.com>
2024-06-09 23:08:16 +0700

    config router, layout

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import { HeaderOnly } from '~/components/Layout';
+import Search from '~/pages/Search';
+const publicRoutes = [
+  { path: '/', componet: Home },
+  { path: '/following', componet: Following },
+  { path: '/profile', componet: Profile },
+  { path: '/upload', componet: Upload, layout: HeaderOnly },
+  { path: '/search', componet: Search, layout: null },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
